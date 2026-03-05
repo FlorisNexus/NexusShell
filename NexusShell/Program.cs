b73bc10cf1ffe9bedbc9b2ac34fcb2e1da6e7478
Florian Di Vrusa <florian.divrusa@inetum.com>
2026-03-05 20:51:04 +0100

    feat: implement Native Executable distribution and GitHub CI with coverage (v12.0)

--- NexusShell/Program.cs
+++ NexusShell/Program.cs
@@ -21,7 +21,7 @@ namespace NexusShell
     /// </summary>
     public class Program
     {
-        private const string APP_VERSION = "v11.7";
+        private const string APP_VERSION = "v12.0";
 
         /// <summary>
         /// Bootstraps the application, configures DI, and starts the UI.
